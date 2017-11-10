199c0cdd85bdc6499a7829eb09065a80dc06ca43
jalynzamora <jalynortiz@yahoo.com>
2017-11-09 21:30:21 -0700

    changes

---
+++ cit230/assignments/lesson-7/scripts/hamburger.js
@@ -0,0 +1,8 @@
+function toggleHam() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topNav";
+    }
+}
