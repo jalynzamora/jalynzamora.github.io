4f6488479256229b81f10e07701a69461c52889f
jalynzamora <jalynortiz@yahoo.com>
2017-12-19 12:52:56 -0500

    :)

---
+++ cit230/assignments/lesson-10/scripts/windchill.js
@@ -0,0 +1,8 @@
+function windChill(temp, speed){
+	var f = Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275  * temp * Math.pow(speed, 0.16));
+	return f;
+}
+ 
+	var result = windChill(40, 5);
+
+	document.getElementById('output').innerHTML = result;
